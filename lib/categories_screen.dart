43ab1f7d4d7c0505bede5d69445b85e8d4a28e8f
Jeremiah Ifeanyichukwu <varisiv@gmail.com>
2020-11-15 19:09:53 +0100

    Add categories_screen.dart and categories

---
+++ lib/categories_screen.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:recipes/category_item.dart';
+import 'package:recipes/dummy_data.dart';
+
+class CategoriesScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('DeliMeals'),
+      ),
+      body: GridView(
+        children: [
+          ...DUMMY_CATEGORIES.map((category) => CategoryItem(category))
+        ],
+        gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+            maxCrossAxisExtent: 200,
+            childAspectRatio: 1.5,
+            crossAxisSpacing: 20,
+            mainAxisSpacing: 20),
+      ),
+    );
+  }
+}
